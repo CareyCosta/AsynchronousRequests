c5e773ec7cf22e5bd1f7e1e938161dc240687a69
Carey Costa <careyxcosta@gmail.com>
2017-02-16 15:51:47 -0500

    step 2 binding to a click event, first button and corresponding ajax successful

--- ajax.js
+++ ajax.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
+  $('.button').on('click', function() {
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/',
+      method:'GET',
+      dataType: 'text'
+    }).done(function(responseData) {
 
-  /* Your code goes here */
+    });
+  });
+  $('.button2').on('click', function() {
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/ping',
+      method:'GET',
+      dataType: 'text'
+    }).done(function(responseData){
 
+    });
+  });
 });
