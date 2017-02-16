b5c76d79a40808a59df7bcf50199cc4b316a2069
Carey Costa <careyxcosta@gmail.com>
2017-02-16 17:07:33 -0500

    step 8 add time button

--- ajax.js
+++ ajax.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
   $('.button-root').on('click', function() {
-
     $.ajax({
       url:'http://first-ajax-api.herokuapp.com/',
       method:'GET',
@@ -10,16 +9,15 @@ $(document).ready(function () {
         console.log('Ajax request sent!');
         $('#step12').append('Ajax request sent!');
     }).fail(function () {
-      console.log('Request Fail! Everybody panic!');
-      $('#step12').append('Request Failed! Errybody Panic!');
+        console.log('Request Fail! Everybody panic!');
+        $('#step12').append('Request Failed! Errybody Panic!');
     }).always(function () {
-      console.log('The request has finished');
-      $('#step12').append('Request has Finished');
+        console.log('The request has finished');
+        $('#step12').append('Request has Finished');
     });
 });
 
   $('.button-ping').on('click', function() {
-
     $.ajax({
       url:'http://first-ajax-api.herokuapp.com/pong',
       method:'GET',
@@ -28,12 +26,48 @@ $(document).ready(function () {
         console.log('Ajax request sent!');
         $('#step3456').append('Ajax request sent!');
     }).fail(function () {
-      console.log('Request Fail! Everybody panic!');
-      $('#step3456').append('Request Failed! Errybody Panic!')
+        console.log('Request Fail! Everybody panic!');
+        $('#step3456').append('Request Failed! Errybody Panic!')
     }).always(function () {
-      console.log('The request has finished');
-      $('#step3456').append('Request has Finished');
+        console.log('The request has finished');
+        $('#step3456').append('Request has Finished');
     });
   });
+
+  $('#step7').on('click', function () {
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/count',
+      method:'GET',
+      dataType: 'text'
+    }).done(function(responseData){
+        console.log('Ajax request sent!');
+        $('.count').text(responseData);
+    }).fail(function () {
+        console.log('Request Fail! Everybody panic!');
+        $('#step7').append('Request Failed! Errybody Panic!')
+    }).always(function () {
+        console.log('The request has finished');
+        $('#step7').append('Request has Finished');
+    });
+  });
+
+  $('#step8').on('click', function () {
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/time',
+      method:'GET',
+      timezone: 'America/Mexico_City',
+      dataType: 'text'
+    }).done(function(responseData){
+        console.log('Ajax request sent!');
+        $('.time').text(responseData);
+    }).fail(function () {
+        console.log('Request Fail! Everybody panic!');
+        $('.time').append('Request Failed! Errybody Panic!')
+    }).always(function () {
+        console.log('The request has finished');
+        $('.time').append('Request has Finished');
+    });
+  });
+
 
 });
