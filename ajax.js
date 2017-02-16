bc52bb195cbaf01af3c063cec2ab083a71e09e08
Carey Costa <careyxcosta@gmail.com>
2017-02-16 16:19:30 -0500

    step 5, add always callback, use console.log to output message

--- ajax.js
+++ ajax.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+
   $('.button-root').on('click', function() {
+
     $.ajax({
       url:'http://first-ajax-api.herokuapp.com/',
       method:'GET',
@@ -7,9 +9,17 @@ $(document).ready(function () {
     }).done(function(responseData) {
         console.log('Ajax request sent!');
         $('#step12').append('Ajax request sent!');
+    }).fail(function () {
+      console.log('Request Fail! Everybody panic!');
+      $('#step12').append('Request Failed! Errybody Panic!');
+    }).always(function () {
+      console.log('The request has finished');
+      $('#step12').append('Request has Finished');
     });
-  });
+});
+
   $('.button-ping').on('click', function() {
+
     $.ajax({
       url:'http://first-ajax-api.herokuapp.com/pong',
       method:'GET',
@@ -20,6 +30,10 @@ $(document).ready(function () {
     }).fail(function () {
       console.log('Request Fail! Everybody panic!');
       $('#step3456').append('Request Failed! Errybody Panic!')
+    }).always(function () {
+      console.log('The request has finished');
+      $('#step3456').append('Request has Finished');
     });
   });
+
 });
